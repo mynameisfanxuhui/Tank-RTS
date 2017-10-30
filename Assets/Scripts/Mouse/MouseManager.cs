708f94b62c6f20e7ab427c580057dca504ddd9c2
Torres Fan <torres.x.fan@pwc.com>
2017-10-30 17:21:47 +0800

    init

---
+++ Assets/Scripts/Mouse/MouseManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	public GameObject go;
+	private List<GameObject> selectedGo;
+	void Start(){
+		selectedGo = new List<GameObject> ();
+        Debug.Log(go.transform.position);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			startPosition = Input.mousePosition;
+			//Debug.Log (startPosition);
+
+		}
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			endPosition = Input.mousePosition;
+			//Debug.Log (endPosition+"end");
+			Rect selectionBox = new Rect(Mathf.Min(startPosition.x, endPosition.x), 
+				Mathf.Min(startPosition.y, endPosition.y),
+				Mathf.Abs(startPosition.x - endPosition.x),
+				Mathf.Abs(startPosition.y - endPosition.y));
+			Vector3 goPosition = Camera.main.WorldToScreenPoint (go.transform.position);
+			//Debug.Log(goPosition+"gameObject position");
+			if (selectionBox.Contains (goPosition))
+			{
+				Debug.Log("selected");
+				selectedGo.Add (go);
+			}
+				
+		}
+		if (Input.GetMouseButtonDown (1)) 
+		{
+            
+			foreach (GameObject selected in selectedGo) 
+			{
+                
+                Vector3 temp = Input.mousePosition;
+                temp.x = Input.mousePosition.x;
+                temp.y = Camera.main.pixelHeight - Input.mousePosition.y;
+
+                Vector3 targetPos = Camera.main.ScreenToWorldPoint(new Vector3(temp.x, temp.y, Camera.main.nearClipPlane));
+                Debug.Log("targetpos="+targetPos);
+                Rigidbody rig = selected.GetComponent<Rigidbody> ();
+                Vector3 dir = (targetPos - selected.transform.position).normalized;
+				//dir.y = 0;
+				Debug.Log ("direction =" + dir);
+                rig.velocity=dir * 10f;
+			}
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;       
+    public Rigidbody m_Shell;            
+    public Transform m_FireTransform;    
+    public Slider m_AimSlider;           
+    public AudioSource m_ShootingAudio;  
+    public AudioClip m_ChargingClip;     
+    public AudioClip m_FireClip;         
+    public float m_MinLaunchForce = 15f; 
+    public float m_MaxLaunchForce = 30f; 
+    public float m_MaxChargeTime = 0.75f;
+
+   
+    private string m_FireButton;
+    private bool isFireBuff;
+    private string m_FireBuffButton;
+    private float m_CurrentLaunchForce;  
+    private float m_ChargeSpeed;         
+    private bool m_Fired;                
+
+
+    private void OnEnable()
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce;
+        m_AimSlider.value = m_MinLaunchForce;
+    }
+
+
+    private void Start()
+    {
+        m_FireButton = "Fire" + m_PlayerNumber;
+        m_FireBuffButton = "FireBuff" + m_PlayerNumber;
+        Debug.Log(m_FireBuffButton);
+        m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+        isFireBuff = false;
+    }
+  
+
+    private void Update()
+    {
+        // Track the current state of the fire button and make decisions based on the current launch force.
+        m_AimSlider.value = m_MinLaunchForce;
+        if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+        {
+            m_CurrentLaunchForce = m_MaxLaunchForce;
+            Fire();
+        }
+        else if (Input.GetButtonDown(m_FireButton))
+        {
+            m_Fired = false;
+            //isFireBuff = false;
+           // Debug.Log("start fire");
+           // Debug.Log(m_Fired);
+            m_CurrentLaunchForce = m_MinLaunchForce;
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+        else if (Input.GetButton(m_FireButton) && !m_Fired)
+        {
+          //  Debug.Log("firing");
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+            m_AimSlider.value = m_CurrentLaunchForce;
+        }
+        else if ( Input.GetButtonDown(m_FireBuffButton))
+        {
+            isFireBuff = true;
+            TankHealth tankHealth = GetComponent<TankHealth>();
+            tankHealth.IsInvincible = true;
+            //tankHealth.CurrentHealth = 100f;
+            Debug.Log(isFireBuff);
+        }
+        else if (Input.GetButtonUp(m_FireButton)&&!m_Fired)
+        {
+            Fire();
+        }
+        
+    }
+
+
+    private void Fire()
+    {
+        // Instantiate and launch the shell.
+        m_Fired = true;
+        Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.transform.position, m_FireTransform.transform.rotation) as Rigidbody;
+        
+        if (isFireBuff)
+        {
+            Debug.Log("FiringggggBuff");
+            ShellExplosion shellExplosion = shellInstance.GetComponent<ShellExplosion>();
+            shellExplosion.m_MaxDamage = 1000f;
+            shellExplosion.m_ExplosionRadius = 20f;
+            
+        }
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+        m_CurrentLaunchForce = m_MinLaunchForce;
+    }
+}
